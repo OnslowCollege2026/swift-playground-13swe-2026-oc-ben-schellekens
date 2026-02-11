5baf5ba185a4bc6561c4d93f1477a0e721276ce3
Ben schellekens <139722670+oc-ben-schellekens@users.noreply.github.com>
2026-02-11 00:00:05 +0000

    I am learning the filtering map reduce

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -4,6 +4,56 @@
 @main
 struct SwiftPlayground {
     static func main() {
-        print("Hello, world!")
+
+        let passingScore = 50
+
+        let numbers: [Int] = [1, 2, 3, 4, 5]
+        let words: [String] = ["apple", "banana", "grape", "strawberry", "kiwi"]
+        let scores: [Int] = [45, 78, 89, 32, 50, 92, 67, 41, 99, 56]
+
+        // Cube the numbers in the list
+        let cubedNumbers: [Int] = numbers.map { $0 * $0 * $0 }
+
+        // Filter for even numbers
+        let evenNumbers: [Int] = cubedNumbers.filter { $0 % 2 == 0 }
+
+        // Add up the numbers to get the sum
+        let numbersSum: Int = evenNumbers.reduce(0) { $0 + $1 }
+
+        // Print out different steps
+        print(cubedNumbers)
+        print(evenNumbers)
+        print(numbersSum)
+
+        // Set words list
+
+        // Reduce the list to the shortest word.
+        let longestWord: String = words.reduce("") { $1.count > $0.count ? $1 : $0 }
+
+        // Print the longest word
+        print(longestWord)
+
+        // Scores input
+
+        // Curve the scores by adding 5
+        let curvedScores = scores.map { $0 + 5 }
+
+        // Filter for passing scores
+        let passingScores = curvedScores.filter { $0 >= passingScore }
+
+        let scoresSum = curvedScores.reduce(0, +)
+
+        let averageScore = scoresSum / passingScores.count
+
+        print(curvedScores)
+        print(passingScores)
+    }
+}
+
+func reduce(_ partialResult: String, _ word: String) -> String {
+    if word.count > partialResult.count {
+        return word
+    } else {
+        return partialResult
     }
 }
