8de6d5ebac0dc830b2d1e8d7836188b3692b06ad
Ben schellekens <139722670+oc-ben-schellekens@users.noreply.github.com>
2026-02-25 01:37:10 +0000

    Finish method tasks (woohoo im all up to date)

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -10,17 +10,47 @@ struct SwiftPlayground {
         let theZook: Book = Book(title: "The Zook", author: "Prof. Z. Zook", pages: .max)
         let theBook: Book = Book(title: "The Book", author: "Prof. B. Book", pages: .min)
 
+        // The method method is cleaner because all of the calling is way easier towrite as you dont have to access every individual peice of data.
         print(theZook.summary())
         print(theBook.summary())
-        print("The method method is cleaner because all of the calling is way easier towrite as you dont have to access every individual peice of data.")
 
+        // The static method is better because it can be used with any number and doesn't require a class to made for one conversion.
         print("22ºF to ºC: \(Temperature.toCelsius(farenheit: 22))")
         print("2147483647ºF to ºC: \(Temperature.toCelsius(farenheit: 2147483647))")
         print("99ºC to ºF: \(Temperature.toFarenheit(celsius: 99))")
 
-        print("The static method is better because it can be used with any number and doesn't require a class to made for one conversion.")
+        var timer: Timer = Timer(seconds: 0, isRunning: false)
 
+        print(timer.info)
 
+        timer.start()
+        print(timer.info)
+        
+
+        while timer.seconds < 10 {
+            timer.tick()
+            print(timer.info)
+        }
+
+        timer.reset()
+        print(timer.info)
+
+        timer.tick()
+        print(timer.info)
+
+        var shoppingCart: Cart = Cart()
+
+        while shoppingCart.itemsCount <= 5 {
+            shoppingCart.addItem()
+            print("Items: \(shoppingCart.itemsCount), \(shoppingCart.shippingMessage)")
+        }
+        
+
+        let badge: Badge = Badge(name: "Ranger", level: 4)
+
+        // It was the same. Source: trust me bro
+        print(badge.label)
+        
     }
 }
 
@@ -52,15 +82,48 @@ struct Timer {
     var seconds: Int 
     var isRunning: Bool
 
-    mutating func start() {
+    var info: String {"Timer \(isRunning ? "running" : "stopped"), at \(seconds)s"}
 
+    mutating func start() {
+        isRunning = true
     }
 
     mutating func tick() {
-
+        if isRunning { seconds += 1 }
     }
 
     mutating func reset() {
-        
+        seconds = 0
+        isRunning = false
+    }
+}
+
+//TODO: Task D comments part
+struct Cart {
+    // These should be the same between all different instances of cart, and also unmodifiable.
+    static let freeShippingThreshold: Int = 5
+    static func qualifiesForFreeShipping(_ count: Int) -> Bool { return count >= freeShippingThreshold }
+
+
+    var itemsCount: Int = 0
+
+    /* 
+    Because addItem() and shippingMessage() interact directly with the data in the struct, they are instance level to be able to use that data
+    */
+    mutating func addItem() {
+        itemsCount += 1
+    }
+
+    var shippingMessage :String {
+        Cart.qualifiesForFreeShipping(itemsCount) ? "Free shipping" : "Shipping applies"
+    }
+}
+
+struct Badge {
+    var name: String
+    var level: Int
+
+    var label: String {
+        "\(name) - Level \(level)"
     }
 }
