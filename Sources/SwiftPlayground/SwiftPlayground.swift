9ba39f7017628fbeaabede89bc7e99f219282383
Ben schellekens <139722670+oc-ben-schellekens@users.noreply.github.com>
2026-03-05 01:21:13 +0000

    some work on protocols

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -6,14 +6,32 @@ import Foundation
 struct SwiftPlayground {
     static func main() {
         let testStudent: Student = Student(id: UUID(), name: "Test", age: 10)
-        let course: Course = Course(
+        let otherTestStudent: Student = Student(id: UUID(), name: "String", age: Int.max)
+        let testCourse: Course = Course(
             id: "SWD13", title: "Software De-engineering",
-            courseDescription: "Learn how to break torins code")
+            courseDescription: "Learn how to break torins code"
+        )
+        let courses: [Course] = [
+            Course(
+                id: "SMC13", title: "Selling miniature campfires",
+                courseDescription: "learn how to sell miniature campfires"),
+            Course(id: "TST12", title: "Tesintehs", courseDescription: "Screaming emoji"),
+            Course(id: "LZY11", title: "Lazy", courseDescription: "thats what i am"),
+        ]
 
-        var testEnrolment: Enrolment = Enrolment(studentID: testStudent.id, courseID: course.id)
+        let testEnrolment: Enrolment = Enrolment(studentID: testStudent.id, courseID: testCourse.id)
 
-        let data = try! JSONEncoder().encode(testEnrolment)
-        let decoded = try! JSONDecoder().decode(Enrolment.self, from: data)
+        let data: Data?
+        let decoded: Enrolment?
+        do { data = try JSONEncoder().encode(testEnrolment) } catch { data = nil }
+
+        do {
+            guard data != nil else {
+                decoded = nil
+                return
+            }
+            decoded = try JSONDecoder().decode(Enrolment.self, from: data!)
+        } catch { decoded = nil }
 
         let scores: [ScoreEntry] = [
             ScoreEntry(studentID: testStudent.id, points: 3),
@@ -21,11 +39,22 @@ struct SwiftPlayground {
             ScoreEntry(studentID: testStudent.id, points: 11),
         ]
 
-        print(scores.sorted { $0 > $1 })
+        print(scores.sorted)
 
-        print(data)
-        print(decoded)
+        print(data!)
+        print(decoded!)
 
+        print(decoded!.hashValue)
+
+        let enrolments: Set<Enrolment> = [
+            Enrolment(studentID: otherTestStudent.id, courseID: courses[0].id),
+            Enrolment(studentID: testStudent.id, courseID: courses[0].id),
+            Enrolment(studentID: testStudent.id, courseID: courses[1].id),
+            Enrolment(studentID: testStudent.id, courseID: courses[1].id),
+            Enrolment(studentID: otherTestStudent.id, courseID: courses[2].id),
+        ]
+
+        print(enrolments)
     }
 }
 
@@ -49,16 +78,15 @@ struct Course: CustomStringConvertible {
     }
 }
 
-struct Enrolment: Codable {
+struct Enrolment: Codable, Hashable {
     let studentID: UUID
     let courseID: String
 }
 
-struct ScoreEntry: Comparable {
+struct ScoreEntry: Comparable, Equatable {
     let studentID: UUID
     var points: Int
 
-    static func < (lhs: ScoreEntry, rhs: ScoreEntry) -> Bool {
-        lhs.points < rhs.points
-    }
+    static func < (lhs: ScoreEntry, rhs: ScoreEntry) -> Bool { lhs.points < rhs.points }
+    static func == (lhs: ScoreEntry, rhs: ScoreEntry) -> Bool { lhs.points == rhs.points }
 }
