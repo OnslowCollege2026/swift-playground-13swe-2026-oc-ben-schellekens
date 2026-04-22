6dc82f155ff966c92ba4ec1ab94b631990f8f59a
Ben schellekens <ben.schellekens@student.onslow.school.nz>
2026-04-22 13:00:38 +1200

    Basic structs for data

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -1,5 +1,40 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+// School library book borrowing system
+// No database (not confident enough)
+//
+
+import Foundation
+
+/// Struct to represent a book
+struct Book: CustomStringConvertible {
+    /// The book's ISBN
+    let id: Int
+
+    /// The title of the book
+    var title: String
+
+    /// The author of the book
+    var author: String
+
+    var description: String {
+        "ISBN: \(id) | \(title) by \(author)"
+        }
+}
+
+/// Struct to represent a borrower
+struct Borrower {
+    /// Borrower ID.
+    let id: UUID
+
+    /// The name of the borrower.
+    var name: String
+}
+
+// Struct to represent a Book loan
+struct Loan {
+    let bookID: Int
+    let borrowerID: UUID
+
+}
 
 @main
 struct SwiftPlayground {
