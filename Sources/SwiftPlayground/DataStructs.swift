2c361c37a48bd294589ace84df1befef6f98695b
Ben schellekens <ben.schellekens@student.onslow.school.nz>
2026-05-01 01:09:40 +1200

    Move data structs (loan, borrower, book) to a seperate file

---
+++ Sources/SwiftPlayground/DataStructs.swift
@@ -0,0 +1,78 @@
+// Structs to store data from the user.
+import Foundation
+
+/// Struct to represent a book
+struct Book: CustomStringConvertible, Identifiable, Codable, Hashable {
+    /// The book's ISBN13
+    let id: Int
+
+    /// The title of the book
+    var title: String
+
+    /// The author of the book
+    var author: String
+
+    var description: String {
+        "ISBN: \(id) | \(title) by \(author)"
+    }
+
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(id)
+    }
+}
+
+/// Struct to represent a borrower
+struct Borrower: CustomStringConvertible, Identifiable, Codable, Hashable {
+    /// Borrower ID.
+    let id: UUID
+
+    /// The name of the borrower.
+    var firstName: String
+
+    // Last name of the borrower
+    var lastName: String
+
+    /// Borrower's date of birth
+    var dateOfBirth: Date
+
+    var age: Int {
+        Int(floor((dateOfBirth.timeIntervalSinceNow * -1) / yearInSeconds))
+    }
+
+    var description: String {
+        "Borrower \(firstName) \(lastName) is \(age) year(s) old (Born \(dateOfBirth))"
+    }
+
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(id)
+    }
+}
+
+/// Struct to represent a Book loan
+struct Loan: CustomStringConvertible, Hashable {
+    /// The book that is being loaned
+    let book: Book
+
+    /// ID of the borrower that is loaning out the book
+    let borrower: Borrower
+
+    /// The date the book needs to be returned by
+    var returnDate: Date
+
+    var paid: Bool = false
+
+    var isOverdue: Bool {
+        returnDate.timeIntervalSinceNow <= 0 && paid == false
+    }
+
+    var description: String {
+        "\(borrower.firstName)'s issued book \(book.title) (ISBN \(book.id)) is due back \(UI.formatDate(returnDate))"
+    }
+
+    // Hash the book ID, borrower ID and return date together.
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(book.id) 
+        hasher.combine(borrower.id)
+        hasher.combine(returnDate)
+    }
+}
