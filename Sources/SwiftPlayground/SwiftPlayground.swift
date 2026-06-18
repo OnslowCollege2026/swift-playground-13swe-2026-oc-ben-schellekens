580f1c04fed8244613d0348575d35e4ccc60284f
Ben schellekens <ben.schellekens@student.onslow.school.nz>
2026-06-19 10:52:41 +1200

    Database structs

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -2,6 +2,104 @@
 import Foundation
 import GRDB
 
+enum BorrowerType: Int, Codable {
+    case staff
+    case student
+}
+
+enum ItemCategory: Int, Codable {
+    case textbooks
+    case manga
+    case readers
+    case flashcardSets
+    case culturalResources
+    case games
+    case electronicGames
+}
+
+enum ItemCondition: Int, Codable {
+    case terrible
+    case poor
+    case fair
+    case good
+    case excellent
+}
+
+struct Borrower: Identifiable, Codable, FetchableRecord, PersistableRecord {
+    var id: Int64? = nil
+    var givenName: String
+    var familyName: String
+    var borrowerType: BorrowerType
+    var yearLevel: Int?
+    var email: String
+
+    // enum CodingKeys: String, CodingKey { 
+    //     var id: Int64? = nil
+    //     var givenName: String
+    //     var familyName: String
+    //     var borrowerType: BorrowerType
+    //     var yearLevel: Int?
+    //     var email: String
+    // }
+
+    enum Columns { 
+        static let id = Column(CodingKeys.id)
+        static let givenName = Column(CodingKeys.givenName)
+        static let familyName = Column(CodingKeys.familyName)
+        static let borrowerType = Column(CodingKeys.borrowerType)
+        static let yearLevel = Column(CodingKeys.yearLevel)
+        static let email = Column(CodingKeys.email)
+    }
+}
+
+struct Item: Identifiable, Codable, FetchableRecord, PersistableRecord {
+    var id: Int64? = nil
+    var itemName: String
+    var itemCategory: ItemCategory
+    var itemCondition: ItemCondition
+
+    // enum CodingKeys: String, CodingKey {
+    //     var id: Int64? = nil
+    //     var itemName: String
+    //     var itemCategory: ItemCategory
+    //     var itemCondition: ItemCondition
+    // }
+
+    enum Columns { 
+        static let id = Column(CodingKeys.id)
+        static let itemName = Column(CodingKeys.itemName)
+        static let itemCategory = Column(CodingKeys.itemCategory)
+        static let itemCondition = Column(CodingKeys.itemCondition)
+    }
+}
+
+struct Loan: Identifiable, Codable, FetchableRecord, PersistableRecord {
+    var id: Int64? = nil
+    var borrowerId: Int64
+    var itemId: Int64
+    var loanDate: Date
+    var dueDate: Date
+    var returnDate: Date?
+
+    // enum CodingKeys: String, CodingKey {
+    //     var id: Int64? = nil
+    //     var borrowerId: Int64
+    //     var itemId: Int64
+    //     var loanDate: Date
+    //     var dueDate: Date
+    //     var returnDate: Date? 
+    // }
+
+    enum Columns {
+        static let id = Column(CodingKeys.id)
+        static let borrowerId = Column(CodingKeys.borrowerId)
+        static let itemId = Column(CodingKeys.itemId)
+        static let loanDate = Column(CodingKeys.loanDate)
+        static let dueDate = Column(CodingKeys.dueDate)
+        static let returnDate = Column(CodingKeys.returnDate)
+    }
+}
+
 @main
 struct SwiftPlayground {
     static func main() {
